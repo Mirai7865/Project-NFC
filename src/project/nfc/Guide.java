b584736d8066e7850ecacfae22e8adaeb50fbcbb
Mirai7865 <1100000436@192.168.56.1>
2021-02-23 13:59:14 -0700

    Now gets article summary from wikipedia website.

--- src/project/nfc/Guide.java
+++ src/project/nfc/Guide.java
@@ -6,6 +6,10 @@
 package project.nfc;
 
 import java.io.File;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import java.util.Scanner;
 
 public class Guide {
 
@@ -20,15 +24,33 @@ public class Guide {
     }
 
     private void loadArticle() {
-        this.article = AccessFile.readFile("data" + File.separator + "articles" + File.separator + this.path + ".txt");
-        this.article = this.article.replaceFirst("@", "");
-        this.article = this.article.replaceAll("@", "\n");
+//        this.article = AccessFile.readFile("data" + File.separator + "articles" + File.separator + this.path + ".txt");
+//        this.article = this.article.replaceFirst("@", "");
+//        this.article = this.article.replaceAll("@", "\n");
+        String urlStr = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Senso-ji";
+        StringBuffer strB = new StringBuffer();
+
+        try {
+            URL url = new URL(new URI(urlStr).toASCIIString());
+            HttpURLConnection con = (HttpURLConnection) (url.openConnection());
+            con.connect();
+            Scanner scr = new Scanner(url.openStream());
+            while (scr.hasNext()) {
+                strB.append(scr.next());
+                strB.append(" ");
+            }
+            scr.close();
+            con.disconnect();
+        } catch (Exception ex) {
+            System.out.println("Unable to grant connection to " + urlStr);
+        }
+        this.article = strB.toString();
     }
 
     public String getArticle() {
         return this.article;
     }
-    
+
     public String getArticleTitle() {
         return this.articleTitle;
     }
