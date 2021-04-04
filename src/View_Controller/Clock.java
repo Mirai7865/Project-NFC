784efa4244abfb4d2090da2af738ca86c3bb61dc
Mirai7865 <60720361+Mirai7865@users.noreply.github.com>
2021-04-04 13:26:47 -0600

    Minor improvements in performance.

--- src/View_Controller/Clock.java
+++ src/View_Controller/Clock.java
@@ -6,38 +6,42 @@
 package View_Controller;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 import java.util.TimeZone;
 import javax.swing.JTextArea;
 
 public class Clock implements Runnable {
-
+    
     private boolean run;
-    public JTextArea textArea;
-
+    private JTextArea textArea;
+    private SimpleDateFormat dateFormat;
+    private Date date;
+    
     public Clock(JTextArea textArea) {
         this.run = true;
         this.textArea = textArea;
+        this.dateFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+        this.dateFormat.setTimeZone(TimeZone.getTimeZone("JST"));
+        this.date = new Date();
     }
-
+    
     @Override
     public void run() {
         while (run) {
             if (!this.run) {
                 return;
             }
-
-            SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
-            dateFormat.setTimeZone(TimeZone.getTimeZone("JST"));
-            Date date = new Date();
-            this.textArea.setText(dateFormat.format(date));
+            this.date = new Date();
+            this.textArea.setText(this.dateFormat.format(this.date));
         }
     }
-
+    
     public void stop() {
         this.run = false;
     }
-
+    
     public boolean getCurrentState() {
         return this.run;
     }
